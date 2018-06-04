b8ba88f2f8a8c4ca876dbc0482917e0f23f3cf3a
Harrylepap <rakotondratompobakoharry@yahoo.fr>
2018-06-04 20:41:30 +0300

    Ajout constructeur et Getters/Setters

--- DebutEnPOO/Program.cs
+++ DebutEnPOO/Program.cs
@@ -8,13 +8,47 @@ namespace DebutEnPOO
 {
     class Voiture
     {
-        public int _vitesse = 50;
-        public int _annee;
-        public string _immatriculation;
-        public string _marque;
+        private int _vitesse = 50;
+        private int _annee;
+        private string _immatriculation;
+        private string _marque;
 
         ///Constructeur
-        
+        public Voiture()
+        {
+            this.Vitesse = 50;
+        }
+        public Voiture(int vitesse)
+        {
+            this.Vitesse = vitesse;
+        }
+        public Voiture(int vitesse, int annee)
+        {
+            this.Annee = annee;
+            this.Vitesse = vitesse;
+        }
+
+        ///Getters and Setters
+        public int Vitesse
+        {
+            get { return _vitesse; }
+            set { _vitesse = value; }
+        }
+        public int Annee
+        {
+            get { return _annee; }
+            set { _annee = value; }
+        }
+        public string Immatriculation
+        {
+            get { return _immatriculation; }
+            set { _immatriculation = value; }
+        }
+        public string Marque
+        {
+            get { return _marque; }
+            set { _marque = value; }
+        }
 
 
         static void Main(string[] args)
